d023acd8bb7e1e1dd04c61860c01755e093d642f
thanhdev1710 <chithanh171004@gmail.com>
2025-03-18 23:09:37 +0700

    fix

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -3,13 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const url = new URL("/login", req.url);
-  (await cookies()).set("token", "", {
-    path: "/",
-    maxAge: 0,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    domain: process.env.NODE_ENV === "production" ? ".kpstudio.vn" : undefined,
-    sameSite: "lax",
-  });
+  (await cookies()).delete("token");
   return NextResponse.redirect(url);
 }
